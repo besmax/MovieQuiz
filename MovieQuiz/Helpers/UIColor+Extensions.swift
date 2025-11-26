d90ee88e4669f6e798f9abb6f84cf810bdcfc74d
bespalovm <bespalovm@radioavionica.ru>
2025-11-26 08:54:27 +0300

    refactoring: move models structs from controller to new files, move mock data to new file

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -4,10 +4,10 @@ extension UIColor {
     /*
      Looks like XCode created the very same extension with colors from assets, so not sure should we keep our duplicate or not
      */
-    static var YpGreen: UIColor { UIColor(named: "yp_green") ?? UIColor.green }
-    static var YpRed: UIColor { UIColor(named: "yp_red") ?? UIColor.red }
-    static var YpBlack: UIColor { UIColor(named: "yp_black") ?? UIColor.black }
-    static var YpBackground: UIColor { UIColor(named: "yp_background") ?? UIColor.darkGray }
-    static var YpGray: UIColor { UIColor(named: "yp_gray") ?? UIColor.gray }
-    static var YpWhite: UIColor { UIColor(named: "yp_white") ?? UIColor.white }
+//    static var YpGreen: UIColor { UIColor(named: "yp_green") ?? UIColor.green }
+//    static var YpRed: UIColor { UIColor(named: "yp_red") ?? UIColor.red }
+//    static var YpBlack: UIColor { UIColor(named: "yp_black") ?? UIColor.black }
+//    static var YpBackground: UIColor { UIColor(named: "yp_background") ?? UIColor.darkGray }
+//    static var YpGray: UIColor { UIColor(named: "yp_gray") ?? UIColor.gray }
+//    static var YpWhite: UIColor { UIColor(named: "yp_white") ?? UIColor.white }
 }
--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -8,7 +8,7 @@ final class MovieQuizViewController: UIViewController {
     
     @IBOutlet weak private var questionLabel: UILabel!
     
-    private let questions: [QuizQuestion] = getQuestions()
+    private let questions: [QuizQuestion] = QuizQuestion.mockData
 
     private var currentQuestionIndex = 0
     private var correctAnswers = 0
@@ -109,67 +109,4 @@ final class MovieQuizViewController: UIViewController {
         alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
     }
-}
-
-struct QuizQuestion {	
-  let image: String
-  let text: String
-  let correctAnswer: Bool
-}
-
-struct QuizStepViewModel {
-  let image: UIImage
-  let question: String
-  let questionNumber: String
-}
-
-struct QuizResultsViewModel {
-  let title: String
-  let text: String
-  let buttonText: String
-}
-
-private func getQuestions() -> [QuizQuestion] {
-    return [
-        QuizQuestion(
-                   image: "The Godfather",
-                   text: "Рейтинг этого фильма больше чем 6?",
-                   correctAnswer: true),
-               QuizQuestion(
-                   image: "The Dark Knight",
-                   text: "Рейтинг этого фильма больше чем 6?",
-                   correctAnswer: true),
-               QuizQuestion(
-                   image: "Kill Bill",
-                   text: "Рейтинг этого фильма больше чем 6?",
-                   correctAnswer: true),
-               QuizQuestion(
-                   image: "The Avengers",
-                   text: "Рейтинг этого фильма больше чем 6?",
-                   correctAnswer: true),
-               QuizQuestion(
-                   image: "Deadpool",
-                   text: "Рейтинг этого фильма больше чем 6?",
-                   correctAnswer: true),
-               QuizQuestion(
-                   image: "The Green Knight",
-                   text: "Рейтинг этого фильма больше чем 6?",
-                   correctAnswer: true),
-               QuizQuestion(
-                   image: "Old",
-                   text: "Рейтинг этого фильма больше чем 6?",
-                   correctAnswer: false),
-               QuizQuestion(
-                   image: "The Ice Age Adventures of Buck Wild",
-                   text: "Рейтинг этого фильма больше чем 6?",
-                   correctAnswer: false),
-               QuizQuestion(
-                   image: "Tesla",
-                   text: "Рейтинг этого фильма больше чем 6?",
-                   correctAnswer: false),
-               QuizQuestion(
-                   image: "Vivarium",
-                   text: "Рейтинг этого фильма больше чем 6?",
-                   correctAnswer: false)
-    ]
 }
